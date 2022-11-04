e92f1eb0411a6f5c3fba5c6279351b6a835207f4
ddthuc <dangthuc.tkmt@gmail.com>
2022-11-04 15:07:10 +0700

    replace github link

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 1; i++) {
-        const img = await faceapi.fetchImage(`http://localhost:8887/img/${label}/${i}.jpg`)
+        const img = await faceapi.fetchImage(`https://kenzoqb3292.github.io/img/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
